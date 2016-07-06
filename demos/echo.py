1e4cc6dd804169b6d8ad1cc25079d3b374cdf63b
Clément Bourgeois <moonpyk@gmail.com>
2016-07-06 22:47:58 +0200

    Is it this ?

--- demos/echo.py
+++ demos/echo.py
@@ -2,12 +2,15 @@
 from __future__ import print_function, unicode_literals
 
 import sys
+
 import subprocess
+
 
 def main(phone, message):
     subprocess.call(["gammu-smsd-inject", "TEXT", phone, "-text", message])
 
 
 if __name__ == '__main__':
-    print([sys.argv[0], sys.argv[1]])
-    main(sys.argv[0], sys.argv[1])
+    argv_ = sys.argv[1:]
+    print(argv_)
+    main(argv_[0], argv_[1])
