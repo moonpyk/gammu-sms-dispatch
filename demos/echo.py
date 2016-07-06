a7b9500ee21663e6f73dab7960c87249de638f0b
Clément Bourgeois <moonpyk@gmail.com>
2016-07-06 22:32:10 +0200

    Encoding stuff

--- demos/echo.py
+++ demos/echo.py
@@ -1,3 +1,6 @@
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
 import sys
 
 import subprocess
@@ -5,6 +8,7 @@ import subprocess
 
 def main(phone, message):
     subprocess.call(["gammu-smsd-inject", "TEXT", phone, "-text", "Coucou je te réponds"])
+
 
 if __name__ == '__main__':
     main(sys.argv[0], sys.argv[1])
