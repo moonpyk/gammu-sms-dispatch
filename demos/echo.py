23d0554c9ec4ecb28345f16becaee7c7fe401afa
Clément Bourgeois <moonpyk@gmail.com>
2016-07-06 22:41:35 +0200

    I think I need more dbg

--- demos/echo.py
+++ demos/echo.py
@@ -2,13 +2,12 @@
 from __future__ import print_function, unicode_literals
 
 import sys
-
 import subprocess
 
-
 def main(phone, message):
-    subprocess.call(["/usr/bin/gammu-smsd-inject", "TEXT", phone, "-text", message])
+    subprocess.call(["gammu-smsd-inject", "TEXT", phone, "-text", message])
 
 
 if __name__ == '__main__':
+    print([sys.argv[0], sys.argv[1]])
     main(sys.argv[0], sys.argv[1])
