4f549f1263ce1092c54e01997bf2f900b3720cc6
Clément Bourgeois <moonpyk@gmail.com>
2016-07-31 21:56:53 +0200

    Added support for admin notifications

--- gammu-dispatch.py
+++ gammu-dispatch.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import os
 import sys
-from ConfigParser import ConfigParser, NoSectionError
+from ConfigParser import ConfigParser, NoSectionError, NoOptionError
 
 import subprocess
 
@@ -49,6 +49,23 @@ def parse_sms():
     }
 
 
+def send_sms(phone, message):
+    # noinspection PyBroadException
+    try:
+        return subprocess.call([
+            'gammu-smsd-inject',
+            'TEXT',
+            phone,
+            '-text',
+            message,
+            '-len',
+            str(len(message))
+        ])
+
+    except:
+        return -1
+
+
 def exec_cmd(syscmd, parsed):
     args = syscmd.split(' ')
     args.append(parsed['phone'])
@@ -58,8 +75,21 @@ def exec_cmd(syscmd, parsed):
     new_env["PHONE"] = parsed['phone']
     new_env["MESSAGE"] = parsed['message']
 
-    if subprocess.call(args, env=new_env) != 0:
-        sys.exit(ERROR_CODES['ERR_DISPATCH_CMD'])
+    return subprocess.call(args, env=new_env)
+
+
+def notify_admin(admin_list, sms, was_parsed, final_code):
+    if len(admin_list) == 0:
+        return
+
+    for phone in admin_list:
+        if len(phone) == 0:
+            continue
+
+        send_sms(
+            phone,
+            '{0} : "{1}" [{2}, {3}]'.format(sms['phone'], sms['message'], was_parsed, final_code)
+        )
 
 
 def main():
@@ -76,15 +106,31 @@ def main():
     if len(parsed) == 0:
         sys.exit(ERROR_CODES['ERR_NO_SMS_FOUND'])
 
+    adm = []
+
+    try:
+        adm = cfg.get('DEFAULT', 'admin').split(';')
+    except NoOptionError:
+        pass
+
+    is_admin = parsed['phone'] in adm
     was_parsed = False
+    final_code = 0
+
     for (order, syscmd) in cmds:
         if parsed["message"].lower() == order:
             was_parsed = True
-            exec_cmd(syscmd, parsed)
+            final_code = exec_cmd(syscmd, parsed)
             break
+
+    if not is_admin:
+        notify_admin(adm, parsed, was_parsed, final_code)
 
     if not was_parsed:
         sys.exit(ERROR_CODES['ERR_NOT_PARSED'])
+
+    if final_code != 0:
+        sys.exit(ERROR_CODES['ERR_DISPATCH_CMD'])
 
 
 if __name__ == '__main__':
