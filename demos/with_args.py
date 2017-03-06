c7785f4959d650bbdc2c491b5a5099fe82abb29d
Clément Bourgeois <moonpyk@gmail.com>
2017-03-06 12:37:43 +0100

    Args

---
+++ demos/with_args.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import os
+
+
+def main(phone, message):
+    # la variable est un tableau qui contient dans chaque cellule les arguments
+    arguments = message.split(' ')[1:]
+
+
+if __name__ == '__main__':
+    main(os.environ["PHONE"], os.environ["MESSAGE"])
